714be5d97c2d22fed23e19e395a6ab29d9d73f6f
Mark Stover <mstover@gospotcheck.com>
2022-02-18 18:09:36 -0700

    add 6-select-timeout

---
+++ 6-select-timeout/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+//
+type Message struct {
+	str  string
+	wait chan bool
+}
+
+// the boring function retuns a 'receive only' channel to communicate with it
+func boring(msg string) <-chan string {
+	c := make(chan string)
+	go func() {
+		for i := 0; ; i++ {
+			c <- fmt.Sprintf("%s %d", msg, i)
+			time.Sleep(time.Duration(rand.Intn(1500)) * time.Millisecond)
+		}
+	}()
+
+	return c
+}
+
+func main() {
+	c := boring("Joe")
+	// timeout limits time select will block for.
+	timeout := time.After(5 * time.Second)
+
+	for {
+		select {
+		case s := <-c:
+			fmt.Println(s)
+		case <-timeout:
+			fmt.Println("You talk too much.")
+			return
+		}
+	}
+}
